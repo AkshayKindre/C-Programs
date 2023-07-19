173a832cf823773e0697a4e53d2f25a812f12d52
Akshay Kindre <110966342+AkshayKindre@users.noreply.github.com>
2023-07-19 10:14:05 +0530

    Add files via upload

---
+++ program54.c
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// Problem : Accept the Number from user and display it in Digits.
+//
+// using for loop.
+// break the code using if condition.
+///////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckDigit(int iNo)
+{
+    int iDigit = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit == 8)
+        {
+            break;
+        }
+        iNo = iNo / 10;
+    }
+    
+    if(iDigit == 8)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    bRet = CheckDigit(iValue);
+    if(bRet == true)
+    {
+        printf("Digit 8 is present in number\n");
+    }
+    else
+    {
+        printf("Digit 8 is not present in number\n");
+    }
+
+    return 0;
+}
