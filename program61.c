2423018f7c3c8621081435a44d1fe548da8e159d
Akshay Kindre <110966342+AkshayKindre@users.noreply.github.com>
2023-07-20 11:41:13 +0530

    Add files via upload

---
+++ program61.c
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Problem : Accept the number and count the Even digits available in that number.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+int CountEvenDigit(int iNo)
+{   
+    int iCount = 0;
+    int iDigit = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if((iDigit % 2) == 0)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    iRet = CountEvenDigit(iValue);
+
+    printf("Frequency of Even digits in %d is %d \n",iValue,iRet);
+
+    return 0;
+}
